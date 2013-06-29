32029933aa95fde1ba26135cd4b9d47fc07cd02e
0871087123 <642238553@qq.com>
2013-06-29 14:18:50 +0800

    modified:   Rasperry PI MOTE/Serial.h 	modified:   Rasperry PI MOTE/function.h 	modified:   Rasperry PI MOTE/test/Makefile 	new file:   Rasperry PI MOTE/test/function_test.cc 	delete:    Rasperry PI MOTE/.dns.cpp.swp 	delete:    Rasperry PI MOTE/test/dns.o 	delete:    Rasperry PI MOTE/test/dns_test.o 	delete:    Rasperry PI MOTE/test/gtest-all.o 	delete:    Rasperry PI MOTE/test/gtest_main.a 	delete:    Rasperry PI MOTE/test/gtest_main.o 	delete:    Rasperry PI MOTE/test/stub.o 	delete:    Rasperry PI MOTE/test/ut

---
+++ Rasperry PI MOTE/function.h
@@ -0,0 +1,22 @@
+/*
+#	这个文件主要包含运行在树莓派上的守护进程。
+#	每隔一段时间，树莓派通过网络向服务器请求新的网络数据。
+#	获取到新的网络数据之后，将文本传输至单片机。
+#	单片机通过使用1602显示对文本进行显示。
+#
+*/
+
+#incllude "basetype.h"
+
+class deamon
+{
+	private:
+		UCHAR advertise[100];
+		ULONG ad_len;
+		ULONG saveAD();
+		ULONG exchangeAD();
+
+	public:
+		deamon();
+		~deamon();
+};
